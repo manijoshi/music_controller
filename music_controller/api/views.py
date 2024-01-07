357f66fd2f1dc3384ed96b60036aba2dbd050687
manijoshi <65389710+manijoshi@users.noreply.github.com>
2024-01-07 18:27:33 +0000

    added model Room

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def main(request):
-    return HttpResponse("<h1>Hello</h1>")
+from rest_framework import generics
+from .models import Room
+from .serializers import RoomSerializer
+# Create your views here
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
